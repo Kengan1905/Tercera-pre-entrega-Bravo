aad547dad8997667d34bad27f3d048def8669279
Kengan <lautarobravo1905@yahoo.com>
2023-06-29 23:40:01 -0300

    Ordenando la estructura de mi static

--- inicio/views.py
+++ inicio/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('<h1>Nuestra primera vista de Tienda</h1>')
+    return render(request, 'inicio/inicio.html')
